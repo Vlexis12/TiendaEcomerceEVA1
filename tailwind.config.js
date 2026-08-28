df7c1bb652457ec44ac6b5586eb238e988eb22d4
Alexis <alexisrozas1507@gmail.com>
2026-08-28 14:34:40 -0400

    Implementacion de Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+    "./tienda/**/*.html",
+    "./admin/**/*.html",
+    "./js/**/*.js" 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [], // Aquí agregarás el plugin de Preline más adelante
+}
+
